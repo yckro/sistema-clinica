637b22b1cc643a367249d008ca8161070a632cae
icaro armando <icaroarmando21@gmail.com>
2024-10-24 16:09:48 -0300

    add component service

--- src/components/service/service.jsx
+++ src/components/service/service.jsx
@@ -1,9 +1,17 @@
 import { View, Text } from "react-native";
+import Button from "../button/button.jsx";
 
-function Service() {
+function Service(props) {
     return <View>
-        <Text>Service</Text>
+        <View>
+            <Text>{props.service}</Text>
+            <Text>{props.price}</Text>
+        </View>
+
+        <View>
+            <Button text="Agendar" />
+        </View>
     </View>
 }
 
-export default Service
+export default Service;
