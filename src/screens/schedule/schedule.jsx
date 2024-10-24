9ae68cb3334babd96298c919e77298403178b4e2
icaro armando <icaroarmando21@gmail.com>
2024-10-24 17:56:10 -0300

    update component schedule

--- src/screens/schedule/schedule.jsx
+++ src/screens/schedule/schedule.jsx
@@ -3,24 +3,50 @@ import { styles } from "./schedule.style.js";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 import { ptBR } from "../../constants/calendar.js";
 import { useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+import Button from "../../components/button/button.jsx";
 
 LocaleConfig.locales['pt-BR'] = ptBR;
 LocaleConfig.defaultLocale = 'pt-BR';
 
 function Schedule() {
 
-    const [selected, setSelected] = useState("");
+    const [selectedDate, setSelectedDate] = useState("");
+    const [selectedHour, setSelectedHour] = useState("");
 
     return <View style={styles.container}>
+
+        <View>
         <Calendar theme={styles.theme}
-            onDayPress={(day) => {setSelected(day.dateString)
+            onDayPress={(day) => {setSelectedDate(day.dateString)
             }}
             markedDates={{
-                [selected]: { selected: true}
+                [selectedDate]: { selected: true}
             }}
 
             minDate={new Date().toDateString()}
         />
+
+        <View>
+            <Text style={styles.textHour}>Horário</Text>
+        </View>
+
+        <View>
+            <Picker
+                selectedValue={selectedHour}
+                onValueChange={(itemValue, itemIndex) => setSelectedHour(itemValue)}
+            >
+                <Picker.Item label="09:00" value="09:00" />
+                <Picker.Item label="10:00" value="10:00" />
+                <Picker.Item label="11:00" value="11:00" />
+            </Picker>
+        </View>
+
+        </View>
+
+        <View>
+            <Button text="Confirmar Reserva" />
+        </View>
 
     </View>
 }
--- src/screens/schedule/schedule.style.js
+++ src/screens/schedule/schedule.style.js
@@ -7,12 +7,21 @@ export const styles = {
         flex: 1,
         paddingLeft: 20,
         paddingRight: 20,
+        justifyContent: "space-between",
+        marginTop: 20,
+        marginBottom: 20
     },
     theme: {
         todayTextColor: COLORS.red,
         selectedDayBackgroundColor: COLORS.blue,
         selectedDayTextColor: COLORS.white,
         arrowColor: COLORS.blue,
+    },
+    textHour: {
+        fontSize: FONT_SIZES.lg,
+        fontWeight: "bold",
+        color: COLORS.gray2,
+        marginTop: 20,
     }
 
 }
