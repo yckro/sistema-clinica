09d1b05bdad2122465452bdce8d089a38960a3b3
icaro armando <icaroarmando21@gmail.com>
2025-01-16 12:48:55 -0300

    add auth user

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import Routes from "./src/routes/routes.js";
 import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./src/contexts/auth.js";
 
 function App() {
-  return (
-    <NavigationContainer>
+
+  return <NavigationContainer>
+    <AuthProvider>
       <Routes />
-    </NavigationContainer>
-  );
+    </AuthProvider>
+  </NavigationContainer>
 }
 
 export default App;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,15 @@
 import { useContext } from "react";
 import RoutesOpen from "./routesOpen.js";
 import RoutesPrivate from "./routesPrivate.js";
+
 import { AuthContext } from "../contexts/auth.js";
 
 function Routes() {
+
     const { user } = useContext(AuthContext);
 
-    // Verifique se user está definido antes de acessar id_user
-    if (!user || !user.id_user) {
-        // Caso o usuário não esteja autenticado ou o valor não seja encontrado, exibe RoutesOpen
-        return <RoutesOpen />;
-    }
-
-    // Se o usuário estiver autenticado, exibe RoutesPrivate
-    return <RoutesPrivate />;
+    return user.id_user ? <RoutesPrivate />
+        : <RoutesOpen />
 }
 
 export default Routes;
